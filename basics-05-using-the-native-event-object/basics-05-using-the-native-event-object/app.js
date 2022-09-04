014d76ad5fe47f00a9dfe55a95609813e8b4b255
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-09-04 16:19:17 -0500

    completed lecture

--- basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
@@ -2,12 +2,50 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: ''
+      //fullName: ''
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // }
+  },
+  computed: {
+    fullName() {
+      console.log('Running again');
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    }
+  },
   methods: {
+    outputFullName() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Singleterry';
+    },
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,6 +53,10 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+    resetName() {
+      this.name = '';
+      this.lastName = '';
     }
   }
 });

cf334736bf0fdfb1d3a71ff70f2d9111b832b051
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-09-08 23:56:27 -0500

    added more logic to game

--- prj-monster-01-starting-setup/prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/prj-monster-01-starting-setup/app.js
@@ -1,20 +1,82 @@
-function getRandomValue()
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0
         }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                return 'Draw';
+            } else if (value <= 0) {
+                return 'You Lose';
+            } 
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                return 'Draw';
+            } else if (value <= 0) {
+                return 'You Win';
+            }
+        }
+    },
+    computed: {
+        lowerPlayerHealth() {
+            return {
+                width: this.playerHealth + '%'
+            }
+        },
+        lowerMonsterHealth() {
+            return {
+                width: this.monsterHealth + '%'
+            }
+        },
+        specialAttackRound() {
+            return this.currentRound % 3 !== 0;
+        },
+        // gameOver() {
+        //     if (this.playerHealth > 0 && this.monsterHealth <= 0) {
+        //         console.log('you win');
+        //         return 'You Win!';
+        //     } else if (this.playerHealth <= 0 && this.monsterHealth > 0 ) {
+        //         return 'You Lose!';
+        //     } else {
+        //         return 'Draw!';
+        //     }
+        // }
     },
     methods: {
         attackMonster() {
-            const attackValue = Math.floor(Math.random() * (12 - 5) + 5);
+            this.currentRound++;
+            const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
+            this.attackPlayer();
         },
         attackPlayer() {
-            const attackValue = Math.floor(Math.random() * (15 - 8) + 8);
+            const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
         }
     },
 });
